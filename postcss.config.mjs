5d03a16473227866bc573885eabad103974af5a6
MertAliuynk <mertuynk3@gmail.com>
2025-11-25 15:42:24 +0300

    tailwind ve post css ayarlandı

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},  // Bu sefer DOĞRU olan bu! (önceki hatayı tersine çevirdik)
-    autoprefixer: {},            // Bunu da ekle, yoksa CSS tarayıcı uyumu bozulur
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
   },
 };
